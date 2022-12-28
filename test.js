c0a0de65949bf297ecc36d4e731d5e042c9d3017
DanielVoric <voricdaniel@gmail.com>
2022-12-28 15:48:52 +0100

    Sređeni gumbovi on/off, dodani alkoholi

--- test.js
+++ test.js
@@ -1,24 +1,168 @@
+
+
+function toggle(el){
+    if(el.className!="play")
+    {
+        el.src='Slike/Votka.jpg';
+        el.className="play";
+    }
+    else if(el.className=="play")
+    {
+        el.src='Slike/Votka.jpg';
+        el.className="pause";
+    }
+    
+    return false;
+}
+
+function toggle1(el){
+    if(el.className!="play")
+    {
+        el.src='Slike/jeger.jpg';
+        el.className="play";
+    }
+    else if(el.className=="play")
+    {
+        el.src='Slike/jeger.jpg';
+        el.className="pause";
+
+    }
+    
+    return false;
+}
+function toggle2(el){
+    if(el.className!="play")
+    {
+        el.src='Slike/tekila.jpg';
+        el.className="play";
+    }
+    else if(el.className=="play")
+    {
+        el.src='Slike/tekila.jpg';
+        el.className="pause";
+
+    }
+    
+    return false;
+}
+
+function toggle3(el){
+    if(el.className!="play")
+    {
+        el.src='Slike/Aperol.jpg';
+        el.className="play";
+    }
+    else if(el.className=="play")
+    {
+        el.src='Slike/Aperol.jpg';
+        el.className="pause";
+
+    }
+    
+    return false;
+}
+
+
+function toggle4(el){
+    if(el.className!="play")
+    {
+        el.src='Slike/gin.jpg';
+        el.className="play";
+    }
+    else if(el.className=="play")
+    {
+        el.src='Slike/gin.jpg';
+        el.className="pause";
+
+    }
+    
+    return false;
+}
+
+
+function toggle5(el){
+    if(el.className!="play")
+    {
+        el.src='Slike/malibu.jpg';
+        el.className="play";
+    }
+    else if(el.className=="play")
+    {
+        el.src='Slike/malibu.jpg';
+        el.className="pause";
+
+    }
+    
+    return false;
+}
+
+
+function toggle6(el){
+    if(el.className!="play")
+    {
+        el.src='Slike/rum.webp';
+        el.className="play";
+    }
+    else if(el.className=="play")
+    {
+        el.src='Slike/rum.webp';
+        el.className="pause";
+
+    }
+    
+    return false;
+}
+
+
+function toggle7(el){
+    if(el.className!="play")
+    {
+        el.src='Slike/viski.jpg';
+        el.className="play";
+    }
+    else if(el.className=="play")
+    {
+        el.src='Slike/viski.jpg';
+        el.className="pause";
+
+    }
+    
+    return false;
+}
+
+
+
+
+
+
+
+
+
+
+/*
 function tgl1() {
     var t = document.getElementById("g1");
     if (t.value == "ON") {
-        t.addEventListener("click", function () {
-            t.className += " inactive";
-            console.log(t.value)
-        })
+        t.className += " inactive";
+        console.log(t.value)
+        console.log("tralala")
+
+        
         t.value = "OFF";
         
         //console.log("unutra sam")
     }
-    else if (t.value == "OFF"); {
-        t.addEventListener("click", function (){
-            t.className += " active";
-            console.log(t.value)
-        })
+    else {
+        t.className += " active";
+        console.log(t.value)
+        console.log("hihiho")
+
         t.value = "ON";
        // console.log("prvi sam")
 
     }
 }
+
 
 function tgl2() {
     var t = document.getElementById("g2");
@@ -60,4 +204,4 @@ function tgl3() {
         console.log("prvi sam")
 
     }
-}
+}*/
